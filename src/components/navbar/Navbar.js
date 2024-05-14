52b79f8d9a4a91271872547f2e803eecd2970436
Madhuri-Giri <madhurigoswami23475@gmail.com>
2024-05-14 15:13:46 +0530

    edit scroll

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,6 +3,7 @@ import "../navbar/Navbar.css"
 import logo from "../../assets/logo.png"
 import Search_icon from "../../assets/search_icon.png"
 import basket_icon from "../../assets/basket_icon.png"
+import { Link } from 'react-router-dom'
 const Navbar = () => {
         // for hover effect
     const [menu, setMenu] = useState("home");
@@ -11,10 +12,10 @@ const Navbar = () => {
     <div className='navbar'>
     <img src={logo} alt='' className='logo'></img>
       <ul className='navbar-item'>
-        <li onClick={()=>setMenu("home")}className={menu==='home'?"active":""} >Home</li>
-        <li onClick={()=>setMenu("menu")}className={menu==='menu'?"active":""} >Menu</li>
-        <li onClick={()=>setMenu("mobile-app")}className={menu==='mobile-app'?"active":""}>mobile-app</li>
-        <li onClick={()=>setMenu("contact-us")}className={menu==='contact-us'?"active":""}>contact-us</li>
+        <Link to='/' onClick={()=>setMenu("home")}className={menu==='home'?"active":""} >Home</Link>
+        <a href='#explore-menu' onClick={()=>setMenu("menu")}className={menu==='menu'?"active":""} >Menu</a>
+        <a href='#app-download' onClick={()=>setMenu("mobile-app")}className={menu==='mobile-app'?"active":""}>mobile-app</a>
+        <a href='#footer' onClick={()=>setMenu("contact-us")}className={menu==='contact-us'?"active":""}>contact-us</a>
       </ul>
         <div className='navbar-right'>
             <img src={Search_icon} alt="" />
